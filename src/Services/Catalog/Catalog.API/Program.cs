df91f4df0555bf0ca4a6b9f5593074e9185a2a4b
Paulo <player_one_117@outlook.com>
2024-08-19 12:45:03 -0500

    Post products working.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
+builder.Services.AddCarter(new DependencyContextAssemblyCatalog(assemblies: typeof(Program).Assembly));
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
-
+app.MapCarter();
 app.Run();
