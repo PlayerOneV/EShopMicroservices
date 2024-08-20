78df85dd40f8b46b077030a3d010e3b419f97aab
Paulo <player_one_117@outlook.com>
2024-08-20 15:16:51 -0500

    Creating CRUD for Products using CQRS and MediatR. To many files in my opinion.

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Products.GetProductByCategory;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+    //public record DeleteProductRequest(Guid id);
+    public record DeleteProductResponse(bool IsSuccess);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public async void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("products/{id:guid}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var response =  result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduc")
+            .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product"); ;
+        }
+    }
+}
